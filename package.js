a340cc624dd5e1d149c67d96c95ed7ac8aebb4b4
Corey Jepperson <corey@entropy.cc>
2015-09-03 11:35:56 -0500

    package fix

--- package.js
+++ package.js
@@ -1,13 +1,11 @@
 Package.describe({
   name: 'jepperct:angular-material-cj',
-  summary: ' A package to add Material Design to Angular ',
-  version: '"0.11.0-rc1-master-ffbcff3-cj',
+  summary: ' A package to add Custom Material Design by CJ to Angular ',
+  version: '0.11.0_2',
   git: 'https://github.com/acoreyj/material'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  //JS Files
   api.addFiles('dist/angular-material.css', 'client');
   api.addFiles('dist/angular-material.js', 'client');
 
